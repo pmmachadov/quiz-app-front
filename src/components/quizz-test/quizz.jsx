7ca6976fe93b8045d47ea8374f0050109456f002
3liezerDev <3dgar3liezer@gmail.com>
2024-05-18 12:06:03 +0200

    edgar feature

---
+++ src/components/quizz-test/quizz.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Quizz = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+Quizz.propTypes = {
+
+};
+
+
+export default Quizz;
