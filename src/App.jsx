3b2e6d55c3fea873e0b3c33e4a99ff1d28aeaf0f
Pablo Machado <100440808+pmmachadov@users.noreply.github.com>
2024-05-24 22:07:45 +0200

    Add TeacherDashboard component and update routing in App.jsx. Questions by topic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home"
+import TeacherDashboard from './components/Dashboard/TeacherDashboard/TeacherDashboard';
 
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      <Route path="/teacher-dashboard" element={ <TeacherDashboard /> } />
+    </Routes>
+  </Router>
+);
 
-function App() {
-
-  return (
-    <>
-  
-      <Home>
-        
-      </Home>
-   
-    </>
-  )
-}
-
-export default App
+export default App;
