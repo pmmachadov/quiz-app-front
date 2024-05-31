a581dbdf1e3630fb1b56121f64038bf89e90e44d
Ruchi <ruchijangra2022@gmail.com>
2024-05-31 15:22:12 -0700

    css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import MainMenu from './components/StartPlay/MainMenu';
-import Quiz from './components/StartPlay/Quiz';
-import EndSession from './components/StartPlay/EndSession';
-import { QuizContext } from './Helpers/Contexts';
+import React, { useState } from 'react'
+import './App.css'
+import MainMenu from './components/StartPlay/MainMenu'
+import Quiz from './components/StartPlay/Quiz'
+import EndSession from './components/StartPlay/EndSession'
+import { QuizContext } from './Helpers/Contexts'
+import './styles/index.css';
 
 function App() {
   const [gameState, setGameState] = useState("menu");
   
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="App flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-center my-4">Quiz App</h1>
       <QuizContext.Provider value={{ gameState, setGameState }}>
         {gameState === "menu" && <MainMenu />}
@@ -16,7 +18,7 @@ function App() {
         {gameState === "endScreen" && <EndSession />}
       </QuizContext.Provider>
     </div>
-  );
+  )
 }
 
 export default App;
